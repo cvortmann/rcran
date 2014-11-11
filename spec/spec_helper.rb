a031d7942cac0e4aa5ca633047fc4f669ab2eb82
Cesar Henrique Vortmann <cvortmann@gmail.com>
2014-11-11 21:07:51 +0100

    Adds celluloid config to specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,20 @@
 require 'mongoid'
 require 'database_cleaner'
+require 'celluloid'
+require 'logger'
 
 ENV['RACK_ENV'] = 'test'
+
 Mongoid.load!("config/mongoid.yml")
+
+Dir.mkdir("log") unless Dir.exists?("log")
+Celluloid.logger = ::Logger.new("log/celluloid.test.log")
 
 RSpec.configure do |config|
   config.before(:each) do
     DatabaseCleaner.start
+    Celluloid.shutdown
+    Celluloid.boot
   end
 
   config.after(:each) do
